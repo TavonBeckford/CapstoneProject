5349da05f2ad988e81b685be5451f3c0942dc67b
Michael Beckford <tavonbeckford@gmail.com>
2021-07-30 16:57:19 -0500

    Edited Generator

--- Database/fake_data.sql
+++ Database/fake_data.sql
@@ -1,46 +1,76 @@
-INSERT INTO Admin(Id,Username,Password) VALUES ('1','EricE','482c811da5d5b4bc6d497ffa98491e38');
+INSERT INTO User(Id,Username,Password,isAdmin) VALUES ('1','MichaelY','482c811da5d5b4bc6d497ffa98491e38','True');
 
-INSERT INTO Officer(Id,Username,Password) VALUES ('1','GregoryM','482c811da5d5b4bc6d497ffa98491e38');
+INSERT INTO User(Id,Username,Password,isAdmin) VALUES ('2','EdwinR','482c811da5d5b4bc6d497ffa98491e38','False');
 
-INSERT INTO Officer(Id,Username,Password) VALUES ('2','JoelM','482c811da5d5b4bc6d497ffa98491e38');
+INSERT INTO User(Id,Username,Password,isAdmin) VALUES ('3','ElizabethL','482c811da5d5b4bc6d497ffa98491e38','False');
 
-INSERT INTO Officer(Id,Username,Password) VALUES ('3','DianaB','482c811da5d5b4bc6d497ffa98491e38');
+INSERT INTO User(Id,Username,Password,isAdmin) VALUES ('4','MatthewM','482c811da5d5b4bc6d497ffa98491e38','False');
 
-INSERT INTO Officer(Id,Username,Password) VALUES ('4','RickyR','482c811da5d5b4bc6d497ffa98491e38');
+INSERT INTO User(Id,Username,Password,isAdmin) VALUES ('5','JohnE','482c811da5d5b4bc6d497ffa98491e38','False');
 
-INSERT INTO Officer(Id,Username,Password) VALUES ('5','CassandraF','482c811da5d5b4bc6d497ffa98491e38');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('9518JK','ARCTIC CAT','F8 EFI SNO PRO','Blue',2008,'fl826376','Coupe','2022-03-21');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('9518JK','CAN-AM','COMMANDER 1000 LTD','Red',2012,'kL073355','Sedan','2022-06-20');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('906792379','Leslie','Perkins','River','78 Red Hills Road Kingston 8 St.Andrew','Jamaica','Hanover','','2000-6-9','Male','9518JK','Jamaica','2022-12-25','Private');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('430946954','Travis','Lee','Arden','41 Hummingbird Cluster Ocho Rios St.Ann','Jamaica','Manchester','testable876@gmail.com','1992-1-19','Female','9518JK','Jamaica','2023-02-4','General');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('8424GR','CHEVROLET','TRAX','Black',2015,'Xm002390','Sedan','2022-01-23');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('8424GR','AUDI','A3 QUATTRO','Black',2007,'QZ090041','Sedan','2022-04-1');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('789217587','Melissa','Ramirez','Bowie','55 Constant Spring Rd Kingston 5 Kingston','Jamaica','Kingston','testable876@gmail.com','1988-10-13','Female','8424GR','Jamaica','2023-03-11','Private');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('666948837','Sarah','Cunningham','River','79 Hummingbird Cluster Ocho Rios St.Ann','Jamaica','Kingston','testable876@gmail.com','1987-1-24','Female','8424GR','Jamaica','2021-08-28','General');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('2445GX','ORION BUS','ORION VII','Black',2003,'pe556476','Sedan','2022-03-10');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('DEPMED','BMW','ALPINA B7L','Red',2014,'YP288107','Sedan','2022-03-20');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('938466446','Andrew','Simmons','River','4 Red Hills Road Kingston 8 St.Andrew','Jamaica','Hanover','testable876@gmail.com','1999-8-11','Male','2445GX','Jamaica','2025-12-25','General');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('891739538','Elizabeth','Pacheco','Belle','69 Hummingbird Cluster Ocho Rios St.Ann','Jamaica','St. Mary','testable876@gmail.com','1964-11-14','Female','DEPMED','Jamaica','2021-01-13','Private');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('1206FQ','INTERNATIONAL','3800','Red',2001,'pv042022','Hatchback','2022-05-17');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('2445GX','FORD','CROWN VICTORIA','Black',2011,'cb156478','Coupe','2022-09-23');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('814022699','Felicia','Thompson','River','43 Red Hills Rd Kingston 19 Kingston','Jamaica','Portland','testable876@gmail.com','1968-5-13','Female','1206FQ','Jamaica','2023-05-7','General');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('368842259','Ricky','Shelton','Claire','62 Constant Spring Rd Kingston 5 Kingston','Jamaica','Hanover','testable876@gmail.com','1984-7-15','Male','2445GX','Jamaica','2023-03-13','Private');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('6606HW','AUDI','R8','Blue',2008,'gB977657','SUV','2022-03-28');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('1206FQ','HONDA','NSS250AS REFLEX SPORT ABS','Blue',2006,'Pi139762','Hatchback','2022-10-19');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('163359762','Jeffrey','Graves','Jude','15 Lady Musgrave Road Kingston 5 Kingston','Jamaica','St. James','testable876@gmail.com','1988-1-24','Male','6606HW','Jamaica','2025-05-19','General');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('589141377','Amanda','Jensen','River','69 Lismore Ave May Pen Clarendon','Jamaica','Portland','testable876@gmail.com','1978-11-27','Female','1206FQ','Jamaica','2025-05-28','Private');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('3840GF','CHEVROLET','SUBURBAN 2500','Red',2007,'vJ185729','Hatchback','2022-08-5');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('6606HW','KAWASAKI','ZX600 NINJA ZX-6R','Blue',2008,'su777496','Coupe','2022-11-16');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('194197648','Caitlin','Phillips','Belle','33 Lady Musgrave Road Kingston 5 Kingston','Jamaica','St. Elizabeth','','1979-2-15','Male','3840GF','Jamaica','2021-08-6','Private');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('902920400','Jose','Stanton','Belle','6 West St Port Antonie Port Antonio','Jamaica','St. Thomas','testable876@gmail.com','1989-12-1','Female','6606HW','Jamaica','2024-02-22','General');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('4737HA','VICTORY','VEGAS','Red',2009,'Gb426140','SUV','2022-07-21');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('3840GF','PETERBILT','337','Black',2011,'yx885195','SUV','2022-02-24');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('166542973','Joy','Johnson','River','87 Manch Rd Kingston 5 Mandeville','Jamaica','Manchester','testable876@gmail.com','1992-6-7','Female','4737HA','Jamaica','2021-07-5','Private');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('172411570','John','Valdez','Claire','15 Manch Rd Kingston 5 Mandeville','Jamaica','St. Catherine','testable876@gmail.com','1987-5-28','Female','3840GF','Jamaica','2022-05-1','General');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('3490GU','CAN-AM','COMMANDER 1000 XT','Black',2014,'Fm793370','SUV','2022-11-10');
 
-INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('4737HA','MASERATI','COUPE','Silver',2004,'PC391286','Coupe','2022-03-6');
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('670017424','Dominique','Harrison','Belle','47 Hummingbird Cluster Ocho Rios St.Ann','Jamaica','Manchester','testable876@gmail.com','1969-1-20','Male','3490GU','Jamaica','2021-04-7','Private');
 
-INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('437299793','Dominique','Clark','Nash','56 John Pringle Dr Hopewell Hanover','Jamaica','Manchester','testable876@gmail.com','1981-10-17','Male','4737HA','Jamaica','2023-09-3','Private');
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('PK0587','KAWASAKI','ZX600 NINJA ZX-6','Red',2001,'AK028930','Hatchback','2022-10-27');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('591902625','Timothy','Harris','Jude','18 Lighthouse Rd Negril Montego Bay','Jamaica','St. Ann','testable876@gmail.com','1968-4-12','Female','PK0587','Jamaica','2022-03-14','General');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('9579JP','APRILIA','RSV4 FACTORY SE','Red',2012,'yk792964','Coupe','2022-04-5');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('405046584','Mary','Weaver','Arden','51 Killarney Rd Ocho Rios St.Ann','Jamaica','Manchester','testable876@gmail.com','1971-12-19','Male','9579JP','Jamaica','2021-03-9','Private');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('2926JC','KAWASAKI','ZX1000 NINJA ZX-10R 30TH ANNIVERSAR','Red',2015,'Sh706732','Coupe','2022-07-16');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('991006985','Alexandra','Dean','Belle','40 Knutsford Blvd Kingston 5 Kingston','Jamaica','St. Thomas','','1990-7-9','Male','2926JC','Jamaica','2025-02-5','Private');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('DC2369','MERCEDES-BENZ','E550','Black',2007,'bI479920','Coupe','2022-11-5');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('601271596','Scott','Brown','Jude','3 John Pringle Dr Hopewell Hanover','Jamaica','St. James','testable876@gmail.com','1991-2-22','Female','DC2369','Jamaica','2021-06-9','Private');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('2889GH','BMW','335I','Blue',2007,'fp228746','Hatchback','2022-01-9');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('691893718','Kimberly','Mathews','River','23 Lismore Ave May Pen Clarendon','Jamaica','St. James','testable876@gmail.com','1995-9-22','Male','2889GH','Jamaica','2021-11-23','Private');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('5543JC','FORD','THUNDERBIRD','Silver',2005,'oN294327','Sedan','2022-05-16');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('845270774','Natasha','Ritter','River','19 John Pringle Dr Hopewell Hanover','Jamaica','St. Ann','testable876@gmail.com','1972-2-22','Female','5543JC','Jamaica','2025-08-8','General');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('5281HE','HUSABERG','TE250','Red',2014,'vy942973','Hatchback','2022-07-25');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('560954310','Jennifer','Cooper','River','81 Angels Walks Rd Spanish Town Saint Catherine','Jamaica','St. Catherine','testable876@gmail.com','1983-7-17','Male','5281HE','Jamaica','2023-09-25','Private');
+
+INSERT INTO Vehicle(LicensePlate,Make,Model,Colour,Year,LicenseDiscNo,Cartype,ExpDate) VALUES ('8139GX','HYUNDAI','SANTA FE XL','Black',2013,'lT514154','SUV','2022-12-12');
+
+INSERT INTO VehicleOwner(TRN,Fname,Lname,Mname,Address,Country,Parish,Email,DOB,Gender,LicensePlate,LicenseIn,ExpDate,LicenseType) VALUES ('326566669','Ryan','Mendez','Bowie','85 Orange St  Montego Bay','Jamaica','Westmoreland','','1968-2-9','Female','8139GX','Jamaica','2024-12-19','General');
 
 INSERT INTO Offence(Code,Description,Fine, Points) VALUES ('F100','Failure to obey traffic signal','5000','8');
 INSERT INTO Offence(Code,Description,Fine, Points) VALUES ('E200','Exceeding the speed limit','7000','12');
@@ -49,11 +79,19 @@ INSERT INTO Location(Description,Parish) VALUES ('48 Old Hope Road, Kingston 7',
 INSERT INTO Location(Description,Parish) VALUES ('9 Darling Street, Kingston 13', 'Kingston');
 INSERT INTO Location(Description,Parish) VALUES ('31B Mona Road, Kingston 5', 'Kingston');
 INSERT INTO Location(Description,Parish) VALUES ('3 Molynes Road, Kingston 4', 'Kingston');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','5','F100','flagged.png');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','3','E200','ja.jpg');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','3','E200','ja2.jpg');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','4','E200','ja3.jpg');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','2','E200','ja4.jpg');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','5','F100','ja5.jpeg');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','2','F100','ja6.jpeg');
-INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-15','17:45:06','5','F100','ja7.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','5','F100','ja.jpg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','5','F100','ja2.jpg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','2','E200','ja3.jpg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','1','E200','ja4.jpg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','4','E200','ja5.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','2','F100','ja6.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','3','E200','ja7.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','2','F100','ja8.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','4','E200','ja9.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','2','E200','ja10.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','5','F100','ja11.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','3','F100','ja12.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','3','E200','ja13.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','3','E200','ja14.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','2','F100','ja15.jpeg');
+INSERT INTO Incident(Date,Time,LocationID,OffenceID,Image) VALUES ('2021-07-30','16:56:41','1','E200','ja16.jpeg');
